00bb6522c2bb7b4cb65d4ec6f7f9f051808c5d87
Renas Reda <renasreda@gmail.com>
2010-07-05 20:10:36 +0200

    Bug fix in pressMenuItem()

--- robotium-solo/src/main/java/com/jayway/android/robotium/solo/Presser.java
+++ robotium-solo/src/main/java/com/jayway/android/robotium/solo/Presser.java
@@ -61,14 +61,14 @@ class Presser{
 				RobotiumUtils.sleep(300);
 				inst.sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_RIGHT);
 			}
-		} else if (index >= 3 && index <= 5) {
+		} else if (index >= 3 && index < 5) {
 			inst.sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_DOWN);	
 			
 			for (int i = 3; i < index; i++) {
 				RobotiumUtils.sleep(300);
 				inst.sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_RIGHT);
 			}
-		} else if (index > 5) {
+		} else if (index >= 5) {
 			inst.sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_DOWN);	
 			inst.sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_DOWN);	
 			
