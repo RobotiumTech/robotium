81ba70f7dbd4ddb5a7faced1b66eb77931f20bea
Hugo Josefson <hugo@josefson.org>
2010-09-21 15:20:41 +0200

    Cleaned up Searcher.searchFor() and removed instance field 'countMatches'.

--- robotium-core/src/main/java/com/jayway/android/robotium/core/impl/Searcher.java
+++ robotium-core/src/main/java/com/jayway/android/robotium/core/impl/Searcher.java
@@ -24,7 +24,6 @@ public class Searcher {
 	private final Instrumentation inst;
     private final Sleeper sleeper;
 	private final int TIMEOUT = 5000;
-	private int countMatches=0;
 	private final String LOG_TAG = "Robotium";
 	
     /**
@@ -130,39 +129,41 @@ public class Searcher {
 	 *
 	 * @param viewClass what kind of {@code View} to search for, e.g. {@code Button.class} or {@code TextView.class}
 	 * @param regex the text to search for. The parameter <strong>will</strong> be interpreted as a regular expression.
-	 * @param matches the number of matches expected to be found. {@code 0} matches means that one or more
+	 * @param expectedNumberOfMatches the number of matches expected to be found. {@code 0} matches means that one or more
 	 * matches are expected to be found.
 	 * @param scroll whether scrolling should be performed
 	 * @return {@code true} if a view of the specified class with the given text is found a given number of times.
 	 * {@code false} if it is not found.
 	 *
 	 */
-	public <T extends TextView> boolean searchFor(Class<T> viewClass, String regex, int matches, boolean scroll) {
+	public <T extends TextView> boolean searchFor(Class<T> viewClass, String regex, int expectedNumberOfMatches, boolean scroll) {
 		sleeper.sleep();
 		inst.waitForIdleSync();
-		Pattern p = Pattern.compile(regex);
-		Matcher matcher;
+
+		if(expectedNumberOfMatches == 0) {
+			expectedNumberOfMatches = 1;
+		}
+		int matchesFound = 0;
+
 		List<T> views = viewFetcher.getCurrentViews(viewClass);
-		if(matches == 0)
-			matches = 1;
+
+		final Pattern pattern = Pattern.compile(regex);
 		for(T view : views){
-			matcher = p.matcher(view.getText().toString());
-			if(matcher.find()){
-				countMatches++;
+			final Matcher matcher = pattern.matcher(view.getText().toString());
+			if (matcher.find()){
+				matchesFound++;
 			}
-			if (countMatches == matches) {
-				countMatches = 0;
+			if (matchesFound == expectedNumberOfMatches) {
 				return true;
 			}
 		}
 
-		if (scroll && scroller.scroll(Scroller.Direction.DOWN))
-		{
-			return searchFor(viewClass, regex, matches, scroll);
+		if (scroll && scroller.scroll(Scroller.Direction.DOWN)) {
+			return searchFor(viewClass, regex, expectedNumberOfMatches, scroll);
 		} else {
-			if (countMatches > 0)
-				Log.d(LOG_TAG, " There are only " + countMatches + " matches of " + regex);
-			countMatches = 0;
+			if (matchesFound > 0) {
+				Log.d(LOG_TAG, " There are only " + matchesFound + " matches of " + regex);
+			}
 			return false;
 		}
 	}
