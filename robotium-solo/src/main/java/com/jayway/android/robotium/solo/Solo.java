c8eaa87cc96b4619eafa76539777ba44e3dcf52b
Renas Reda <renasreda@gmail.com>
2010-09-28 18:39:56 +0200

    Updated getViews() javadoc

--- robotium-solo/src/main/java/com/jayway/android/robotium/solo/Solo.java
+++ robotium-solo/src/main/java/com/jayway/android/robotium/solo/Solo.java
@@ -108,10 +108,10 @@ public class Solo {
 	}
 
 	/**
-	 * Returns a {@code List} of the {@code View}s located in the current
+	 * Returns an {@code ArrayList} of the {@code View}s located in the current
 	 * {@code Activity}.
 	 *
-	 * @return a {@code List} of the {@code View}s located in the current {@code Activity}
+	 * @return an {@code ArrayList} of the {@code View}s located in the current {@code Activity}
 	 *
 	 */
 	
