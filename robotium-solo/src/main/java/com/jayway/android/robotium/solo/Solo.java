3734cabd9e7263c45b47535ebec0e3499b3810d2
Renas Reda <renasreda@gmail.com>
2010-09-28 18:56:17 +0200

    Updated getCurrentGridViews() Javadoc

--- robotium-solo/src/main/java/com/jayway/android/robotium/solo/Solo.java
+++ robotium-solo/src/main/java/com/jayway/android/robotium/solo/Solo.java
@@ -1043,10 +1043,10 @@ public class Solo {
 	}
 	
 	/**
-	 * Returns a {@code List} of the {@code GridView}s contained in the current
+	 * Returns an {@code ArrayList} of the {@code GridView}s contained in the current
 	 * {@code Activity}.
 	 *
-	 * @return a {@code List} of the {@code GridView}s contained in the current
+	 * @return an {@code ArrayList} of the {@code GridView}s contained in the current
 	 * {@code Activity}
 	 *
 	 */
