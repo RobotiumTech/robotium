9363cafbb25bd32ff8d9bf13dcf47f684582c3d1
sascanagl <carl.nagle@sas.com>
2012-04-02 18:13:45 -0300

    Update robotium-solo/src/main/java/com/jayway/android/robotium/solo/ViewFetcher.java

--- robotium-solo/src/main/java/com/jayway/android/robotium/solo/ViewFetcher.java
+++ robotium-solo/src/main/java/com/jayway/android/robotium/solo/ViewFetcher.java
@@ -42,7 +42,7 @@ class ViewFetcher {
 
 	public View getTopParent(View view) {
 		if (view.getParent() != null
-				&& !view.getParent().getClass().getName().equals("android.view.ViewRoot")) {
+				&& view.getParent() instanceof android.view.View) {
 			return getTopParent((View) view.getParent());
 		} else {
 			return view;
