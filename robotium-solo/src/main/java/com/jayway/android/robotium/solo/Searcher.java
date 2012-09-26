1f8b095e3ff9dcbd8102c42633d58d53dfa62f5f
Andy Clark <andy.clark@realvnc.com>
2012-09-26 18:31:27 +0100

    Remove the sleep that always precedes the fetching of views
    The only time that this callback is called is at the start of a 'wait
    for timeout' loop in searchFor(Callable, String, int, etc...). As such,
    if the first thing that happens is a sleep(), the search is always
    delayed by the duration of that sleep, even if the search target is
    already present. 
    
    As the 'wait for timeout' loop in question already has a sleep in an 
    appropriate place, the sleep in the callback is unnecessary.

--- robotium-solo/src/main/java/com/jayway/android/robotium/solo/Searcher.java
+++ robotium-solo/src/main/java/com/jayway/android/robotium/solo/Searcher.java
@@ -93,8 +93,6 @@ class Searcher {
 	public <T extends TextView> boolean searchFor(final Class<T> viewClass, final String regex, final int expectedMinimumNumberOfMatches, final boolean scroll, final boolean onlyVisible) {
 		final Callable<Collection<T>> viewFetcherCallback = new Callable<Collection<T>>() {
 			public Collection<T> call() throws Exception {
-				sleeper.sleep();
-
 				if(onlyVisible)
 					return RobotiumUtils.removeInvisibleViews(viewFetcher.getCurrentViews(viewClass));
 
