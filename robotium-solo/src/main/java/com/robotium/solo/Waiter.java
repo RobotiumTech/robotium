ccf717752340c924c6c87a7bedbfef7d61c3b837
splatte <sp@x-berg.de>
2014-05-05 22:44:39 +0200

    fix: Exception doesn't catch NoSuchMethodError, Throwable does

--- robotium-solo/src/main/java/com/robotium/solo/Waiter.java
+++ robotium-solo/src/main/java/com/robotium/solo/Waiter.java
@@ -715,7 +715,7 @@ class Waiter {
 				return activityUtils.getCurrentActivity().getFragmentManager().findFragmentById(id);
 			else
 				return activityUtils.getCurrentActivity().getFragmentManager().findFragmentByTag(tag);
-		}catch (Exception ignored) {}
+		}catch (Throwable ignored) {}
 
 		return null;
 	}
